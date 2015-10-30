f23c441a98eda49965fb04c3c97ebfa7b4c64f9e
FlashHK <80854284@qq.com>
2015-10-30 11:34:27 +0800

    修改例子数据

--- example/src/index.js
+++ example/src/index.js
@@ -1,5 +1,5 @@
 var helpers = require('./template-helpers.js');
-var data = {list:[{title : "a"},{title : "b"}]};
+var data = {list:[{time : 1446175992278},{time : 1446176021568}]};
 document.getElementById('app').innerHTML = require('./a/a.tpl')(data);
 
 
